e256c022d6a7aef374b596ad1c8cf62bba356450
Stephanie Lenja <109711265+Koci17@users.noreply.github.com>
2022-07-23 12:37:11 +0800

    Update irisjuly.py

--- irisjuly.py
+++ irisjuly.py
@@ -37,7 +37,6 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
 
 st.dataframe(df)  # Same as st.write(df)
          

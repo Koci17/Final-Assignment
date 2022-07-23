51d1946e85e4dfa7aeda58ae704b7f6f8a561b55
Stephanie Lenja <109711265+Koci17@users.noreply.github.com>
2022-07-23 12:59:24 +0800

    Update irisjuly.py

--- irisjuly.py
+++ irisjuly.py
@@ -1,58 +1,51 @@
 import streamlit as st
-	import pandas as pd
-	from sklearn.ensemble import RandomForestClassifier
-	
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
-	st.write("""
-	# Simple Iris Flower Prediction App
-	This app predicts the **Iris flower** type!
-	""")
-	
+st.write("""
+# Simple Iris Flower Prediction App
+This app predicts the **Iris flower** type!
+""")
 
-	st.sidebar.header('User Input Parameters')
-	
+st.sidebar.header('User Input Parameters')
 
-	def user_input_features():
-	    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
-	    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
-	    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-	    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
-	    data = {'sepal_length': sepal_length,
-	            'sepal_width': sepal_width,
-	            'petal_length': petal_length,
-	            'petal_width': petal_width}
-	    features = pd.DataFrame(data, index=[0])
-	    return features
-	df = user_input_features()
-	
+def user_input_features():
+    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
+    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
+    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
+    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
+    data = {'sepal_length': sepal_length,
+            'sepal_width': sepal_width,
+            'petal_length': petal_length,
+            'petal_width': petal_width}
+    features = pd.DataFrame(data, index=[0])
+    return features
 
-	st.subheader('User Input parameters')
-	st.write(df)
-	
+df = user_input_features()
 
-	iris = pd.read_csv('https://raw.githubusercontent.com/ismailbahrudin/iris-july/main/IRIS.csv')
-	X = iris.drop('species',axis = 1)
-	Y = iris['species']
-	
+st.subheader('User Input parameters')
+st.write(df)
 
-	clf = RandomForestClassifier()
-	clf.fit(X, Y)
-	
+iris = pd.read_csv('https://raw.githubusercontent.com/ismailbahrudin/iris-july/main/IRIS.csv')
+X = iris.drop('species',axis = 1)
+Y = iris['species']
 
-	prediction = clf.predict(df)
-	prediction_proba = clf.predict_proba(df)
-	
+clf = RandomForestClassifier()
+clf.fit(X, Y)
 
-	st.subheader('Class labels and their corresponding index number')
-	
-    st.table(df)
-	
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
 
-	         
-	st.subheader('Prediction')
-	#st.write(iris.target_names[prediction])
-	st.write(prediction)
-	
+st.subheader('Class labels and their corresponding index number')
 
-	st.subheader('Prediction Probability')
-	st.write(prediction_proba)
+    
+ st.table(df)
+
+st.dataframe(df)  # Same as st.write(df)
+         
+st.subheader('Prediction')
+#st.write(iris.target_names[prediction])
+st.write(prediction)
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
